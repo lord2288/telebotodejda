8d2d6128b94a649909a3ee8561803ad5dd14e4ef
trolololo22 <mr.danila.pavlov@gmail.com>
2024-05-17 21:59:11 +0300

    1

--- rev2.py
+++ rev2.py
@@ -7,7 +7,6 @@ import time
 
 bot = telebot.TeleBot('6751306928:AAFjUwlLjeMQWqBkauV1NbbXvvTnJRo4fGg')
 
-
 def generate_image_from_text(prompt):
     url = 'https://api-key.fusionbrain.ai/'
     api_key = 'C620606DC592BF60C4462E27F5309E12'
@@ -57,20 +56,22 @@ def generate_image_from_text(prompt):
         time.sleep(10)
     return None
 
-
 def send_picture(message, filename):
     with open(filename, "rb") as photo:
         bot.send_photo(message.chat.id, photo)
 
-
 def pre0(message):
-    global zapros
+    global zapros, available_clothing_items
     zapros = ''
+    available_clothing_items = {
+        'Верхняя одежда': ['Куртка', 'Плащ', 'Жилетка', 'Ветровка', 'Поло', 'Свитер', 'Блузка', 'Рубашка', 'Свитшот', 'Водолазка', 'Топ'],
+        'Нижняя одежда': ['Шорты', 'Брюки', 'Юбка', 'Джинсы'],
+        'Обувь': ['Кроссовки', 'Туфли', 'Лоферы', 'Сапоги', 'Ботинки']
+    }
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.row(types.KeyboardButton('Мужская'), types.KeyboardButton('Женская'))
     bot.send_message(message.chat.id, 'Выберите пол на кого хотите сгенерировать одежду.', reply_markup=markup)
     bot.register_next_step_handler(message, pre1)
-
 
 def pre1(message):
     global zapros
@@ -85,99 +86,73 @@ def pre1(message):
     else:
         pre0(message)
 
-
 def pre2(message):
     global zapros
     zapros += f'в {message.text} Стиле, '
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    categories = ['Верхняя одежда-пальто', 'Верхняя одежда-футболка', 'Нижняя одежда-шорты']
+    categories = ['Верхняя одежда', 'Нижняя одежда', 'Обувь']
     for category in categories:
-        markup.add(types.KeyboardButton(category))
+        if available_clothing_items[category]:  # Only add categories that have available items
+            markup.add(types.KeyboardButton(category))
     bot.send_message(message.chat.id, 'Выберите категорию одежды.', reply_markup=markup)
     bot.register_next_step_handler(message, pre3)
 
-
 def pre3(message):
-    global zapros
+    global zapros, selected_category
+    selected_category = message.text
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    if message.text == 'Верхняя одежда-пальто':
-        items = ['Куртка', 'Плащ', 'Жилетка', 'Ветровка']
-    elif message.text == 'Верхняя одежда-футболка':
-        items = ['Поло', 'Свитер', 'Блузка', 'Рубашка', 'Свитшот', 'Водолазка', 'Топ']
-    elif message.text == 'Нижняя одежда-шорты':
-        items = ['Шорты', 'Брюки', 'Юбка', 'Джинсы']
+    if selected_category in available_clothing_items:
+        items = available_clothing_items[selected_category]
+        for item in items:
+            markup.add(types.KeyboardButton(item))
+        zapros += f'{selected_category}: '
+        bot.send_message(message.chat.id, 'Выберите конкретный элемент одежды.', reply_markup=markup)
+        bot.register_next_step_handler(message, pre4)
     else:
         pre2(message)
-        return
-    for item in items:
-        markup.add(types.KeyboardButton(item))
-    zapros += f'{message.text}, '
-    bot.send_message(message.chat.id, 'Выберите конкретный элемент одежды.', reply_markup=markup)
-    bot.register_next_step_handler(message, pre4)
-
 
 def pre4(message):
-    global zapros
-    zapros += f'{message.text}, '
-    markup = types.ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, 'Напишите цвет одежды.', reply_markup=markup)
+    global zapros, available_clothing_items, selected_category
+    selected_item = message.text
+    zapros += f'{selected_item}, '
+    available_clothing_items[selected_category].remove(selected_item)  # Remove selected item from available items
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton('Готово'), types.KeyboardButton('Добавить еще один элемент одежды'))
+    bot.send_message(message.chat.id, 'Напишите цвет одежды или добавьте еще один элемент.', reply_markup=markup)
     bot.register_next_step_handler(message, pre5)
-
 
 def pre5(message):
     global zapros
-    zapros += f'Цвет {message.text}, '
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    footwear = ['Кроссовки', 'Туфли', 'Лоферы', 'Сапоги', 'Ботинки']
-    for item in footwear:
-        markup.add(types.KeyboardButton(item))
-    bot.send_message(message.chat.id, 'Выберите обувь.', reply_markup=markup)
-    bot.register_next_step_handler(message, pre6)
-
-
-def pre6(message):
-    global zapros
-    zapros += f'На ногах {message.text}.'
-    markup = types.ReplyKeyboardRemove()  # Remove buttons before generation
-    bot.send_message(message.chat.id, 'Подождите немного... Генерация изображения может составлять минуту',
-                     reply_markup=markup)
-
-    filename = generate_image_from_text(zapros)
-    if filename:
-        send_picture(message, filename)
-
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # Prepare the markup for the actions after generation
-    markup.row(types.KeyboardButton('Перегенерировать'), types.KeyboardButton('Задать вид заново'))
-    bot.send_message(message.chat.id, 'Выберите действие.', reply_markup=markup)
-    bot.register_next_step_handler(message, pre7)
-
+    print(zapros)
+    if message.text == 'Готово':
+        bot.send_message(message.chat.id, 'Подождите немного... Генерация изображения может составлять минуту')
+        filename = generate_image_from_text(zapros)
+        if filename:
+            send_picture(message, filename)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row(types.KeyboardButton('Перегенерировать'), types.KeyboardButton('Задать вид заново'))
+        bot.send_message(message.chat.id, 'Выберите действие.', reply_markup=markup)
+        bot.register_next_step_handler(message, pre7)
+    else:
+        zapros += f'Цвет {message.text}, '
+        pre2(message)
 
 def pre7(message):
     global zapros
     if message.text == 'Перегенерировать':
-        markup = types.ReplyKeyboardRemove()  # Remove buttons before regeneration
-        bot.send_message(message.chat.id, 'Подождите немного... Генерация изображения может составлять минуту',
-                         reply_markup=markup)
-        print(zapros)
+        bot.send_message(message.chat.id, 'Подождите немного... Генерация изображения может составлять минуту')
         filename = generate_image_from_text(zapros)
         if filename:
             send_picture(message, filename)
-
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # Restore the action buttons
-        markup.row(types.KeyboardButton('Перегенерировать'), types.KeyboardButton('Задать вид заново'))
-        bot.send_message(message.chat.id, 'Выберите действие.', reply_markup=markup)
-        bot.register_next_step_handler(message, pre7)
     elif message.text == 'Задать вид заново':
         zapros = ''
         pre0(message)
     else:
-        pre6(message)
-
+        pre7(message)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     pre0(message)
-
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
@@ -186,6 +161,5 @@ def handle_photo(message):
     file_path = file_info.file_path
     downloaded_file = bot.download_file(file_path)
     bot.send_photo(message.chat.id, downloaded_file)
-
 
 bot.polling(none_stop=True)
