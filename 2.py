14e3312f38b3dc9b79a33de463bac110295a96ac
trolololo22 <mr.danila,pavlov@gmail.com>
2024-05-28 23:12:29 +0300

    1

--- 2.py
+++ 2.py
@@ -1,5 +1,4 @@
 import logging
-
 import telebot
 from telebot import types
 import base64
@@ -10,8 +9,13 @@ import sqlite3 as sq
 import datetime
 import key
 
+# Логирование
+logging.basicConfig(level=logging.INFO)
 
+# Создание экземпляра бота
 bot = telebot.TeleBot(key.telebot_api)
+
+# Инициализация базы данных
 def init_db():
     with sq.connect('olat.db') as db:
         db.execute('''CREATE TABLE IF NOT EXISTS subscriptions (
@@ -20,10 +24,14 @@ def init_db():
             date_end TEXT,
             mail TEXT
         )''')
+
+# Проверка подписки
 def is_subscribed(user_id):
     with sq.connect('olat.db') as db:
         result = db.execute("SELECT COUNT(*) FROM subscriptions WHERE id=?", (user_id,)).fetchone()
         return result[0] > 0
+
+# Регистрация подписки
 def register_subscription(user_id):
     with sq.connect('olat.db') as db:
         date_start = datetime.date.today()
@@ -31,20 +39,26 @@ def register_subscription(user_id):
                     else date_start.replace(year=date_start.year + 1, month=1))
         db.execute("INSERT INTO subscriptions (id, date_start, date_end, mail) VALUES (?, ?, ?, ?)",
                    (user_id, date_start.isoformat(), date_end.isoformat(), None))
+
+# Количество дней до окончания подписки
 def days_left(user_id):
     with sq.connect('olat.db') as db:
         result = db.execute("SELECT date_end FROM subscriptions WHERE id=?", (user_id,)).fetchone()
         date_end = datetime.datetime.strptime(result[0], '%Y-%m-%d').date()
         return (date_end - datetime.date.today()).days
+
+# Генерация изображения по тексту
 def generate_image_from_text(prompt):
     try:
         url = 'https://api-key.fusionbrain.ai/'
         api_key = 'C620606DC592BF60C4462E27F5309E12'
         secret_key = '40D31C36DFDBD37DD3C385A7B5C143B4'
         headers = {'X-Key': f'Key {api_key}', 'X-Secret': f'Secret {secret_key}'}
+
         response = requests.get(f"{url}key/api/v1/models", headers=headers)
         response.raise_for_status()
         model_id = response.json()[0]['id']
+
         params = {
             "type": "GENERATE",
             "numImages": 1,
@@ -59,6 +73,7 @@ def generate_image_from_text(prompt):
         response = requests.post(f"{url}key/api/v1/text2image/run", headers=headers, files=data)
         response.raise_for_status()
         uuid = response.json()['uuid']
+
         for _ in range(10):
             response = requests.get(f"{url}key/api/v1/text2image/status/{uuid}", headers=headers)
             response.raise_for_status()
@@ -72,11 +87,15 @@ def generate_image_from_text(prompt):
             time.sleep(10)
         return None
     except Exception as e:
-        print(f"Error generating image: {e}")
+        logging.error(f"Error generating image: {e}")
         return None
+
+# Отправка изображения
 def send_image(chat_id, filename):
     with open(filename, "rb") as photo:
         bot.send_photo(chat_id, photo)
+
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
     # user_id = message.chat.id
@@ -88,39 +107,42 @@ def start(message):
     #     bot.send_message(user_id, 'вы не купили подписку', reply_markup=markup)
     #     bot.register_next_step_handler(message, handle_subscription)
     bot.register_next_step_handler(message, show_main_menu)
+
+# Обработчик всех сообщений
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.send_message(message.chat.id, 'Извините за предоставленные неудобства, начните пожалуйста заново.')
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    options = ["Начать генерация", "Tex.поддежка"]
-    markup.add(*[types.KeyboardButton(option) for option in options])
-    bot.send_message(message.chat.id, 'Выберите действие.', reply_markup=markup)
     show_main_menu(message)
-def handle_error(message, error_text, handler):
-    bot.send_message(message.chat.id, f"Произошла ошибка: {error_text}. Пожалуйста, попробуйте снова.")
-    handler(message)
+
+# Обработка подписки
 def handle_subscription(message):
     if message.text == 'купить подписку':
         register_subscription(message.chat.id)
         show_main_menu(message)
+
+# Показ главного меню
 def show_main_menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    options = ["Начать генерация", "Tex.поддежка"]
+    options = ["Начать генерация", "Tex.поддержка"]
     markup.add(*[types.KeyboardButton(option) for option in options])
     bot.send_message(message.chat.id, 'Выберите действие.', reply_markup=markup)
     bot.register_next_step_handler(message, handle_main_menu)
+
+# Обработка главного меню
 def handle_main_menu(message):
     if message.text == "Начать генерация":
         start_generation(message)
-    elif message.text == "Информация о подипске":
+    elif message.text == "Информация о подписке":
         bot.send_message(message.chat.id, f'Осталось дней: {days_left(message.chat.id)}')
         show_main_menu(message)
-    elif message.text == "Tex.поддежка":
-        bot.send_message(message.chat.id, key.email)
+    elif message.text == "Tex.поддержка":
+        bot.send_message(message.chat.id, 'здесь mail человека')
         show_main_menu(message)
     else:
         bot.send_message(message.chat.id, 'неизвестная команда')
         show_main_menu(message)
+
+# Начало генерации
 def start_generation(message):
     global request_data
     request_data = {'gender': '', 'style': '', 'clothing': []}
@@ -128,6 +150,8 @@ def start_generation(message):
     markup.row(types.KeyboardButton('Мужская'), types.KeyboardButton('Женская'))
     bot.send_message(message.chat.id, 'Выберите пол.', reply_markup=markup)
     bot.register_next_step_handler(message, choose_gender)
+
+# Выбор пола
 def choose_gender(message):
     if message.text in ['Мужская', 'Женская']:
         request_data['gender'] = message.text.lower()
@@ -139,24 +163,32 @@ def choose_gender(message):
     else:
         bot.send_message(message.chat.id, 'Неверный выбор пола.')
         start_generation(message)
+
+# Выбор стиля
 def choose_style(message):
     request_data['style'] = message.text
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    categories = ['Верхняя одежда', 'Низ', 'Обувь']
+    categories = ['Верхняя одежда', 'Нижняя одежда', 'Обувь']
     markup.add(*[types.KeyboardButton(category) for category in categories])
     bot.send_message(message.chat.id, 'Выберите категорию одежды.', reply_markup=markup)
     bot.register_next_step_handler(message, choose_category)
+
+# Выбор категории
 def choose_category(message):
     request_data['category'] = message.text
     items = {
-        'Верхняя одежда': ['Куртка', 'Плащ', 'Жилетка', 'Ветровка', 'Поло', 'Свитер', 'Блузка', 'Рубашка', 'Свитшот', 'Водолазка', 'Топ'],
-        'Низ': ['Шорты', 'Брюки', 'Юбка', 'Джинсы'],
+        'Верхняя одежда': ['Куртка', 'Плащ', 'Жилетка', 'Ветровка', 'Поло', 'Свитер', 'Блузка', 'Рубашка', 'Свитшот',
+                           'Водолазка', 'Топ'],
+        'Нижняя одежда': ['Шорты', 'Брюки', 'Юбка', 'Джинсы'],
         'Обувь': ['Кроссовки', 'Туфли', 'Лоферы', 'Сапоги', 'Ботинки']
     }
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(*[types.KeyboardButton(item) for item in items.get(message.text, [])])
-    bot.send_message(message.chat.id, 'Выберите элемент одежды', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Выберите элемент одежды или нажмите "Готово" для завершения.',
+                     reply_markup=markup)
     bot.register_next_step_handler(message, choose_item)
+
+
 def choose_item(message):
     if message.text == 'Готово':
         finalize_selection(message)
@@ -164,18 +196,24 @@ def choose_item(message):
         request_data['clothing'].append(message.text)
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         markup.row(types.KeyboardButton('Готово'), types.KeyboardButton('Добавить еще один элемент одежды'))
-        bot.send_message(message.chat.id, 'Выберите элемент одежды или нажмите "Готово" для завершения.', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Напишите цвет одежды или добавьте еще один элемент.', reply_markup=markup)
         bot.register_next_step_handler(message, finalize_or_add)
+
+
 def finalize_or_add(message):
     if message.text == 'Готово':
         finalize_selection(message)
     elif message.text == 'Добавить еще один элемент одежды':
         choose_style(message)
     # else:
-    #     request_data['color'] = message.text
-    #     choose_category(message)
+    #     bot.send_message(message.chat.id, 'Неверный выбор.')
+    #     finalize_or_add(message)
+
+# Завершение выбора и генерация изображения
 def finalize_selection(message):
     generate_and_send_image(message)
+
+# Генерация и отправка изображения
 def generate_and_send_image(message):
     bot.send_message(message.chat.id, 'Подождите, идет генерация изображения...')
     prompt = f"Изобразите {request_data['gender']} в стиле {request_data['style']} с одеждой: {', '.join(request_data['clothing'])}"
@@ -184,6 +222,7 @@ def generate_and_send_image(message):
         send_image(message.chat.id, filename)
     show_main_menu(message)
 
+# Основная функция
 def main():
     init_db()
     bot.polling(none_stop=True, interval=2)
---
+++ key.py
@@ -0,0 +1,3 @@
+telebot_api = '6751306928:AAFjUwlLjeMQWqBkauV1NbbXvvTnJRo4fGg'
+email_pass = '2jLNZXXm'
+email = 'fogtelebot@gmail.com'
